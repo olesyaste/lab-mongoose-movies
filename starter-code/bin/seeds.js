0683ddd2b46bc98e9fbd69702a4314610e05647c
Olesya Steinmetz <newsabome@icloud.com>
2020-09-07 22:05:43 +0200

    WDFT-Berlin-Olesya

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Celebrity = require('../models/Celebrity');
+
+mongoose.connect('mongodb://localhost/starter-code', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const celebrities = [
+  {
+    name: "Tom Cruise",
+    occupation: "Actor",
+    catchPhrase:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    name: "Beyonce",
+    occupation: "Singer",
+    catchPhrase:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    name: "Kim Kardashian",
+    occupation: "Tv-host",
+    catchPhrase:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  }
+]
+
+Celebrity.insertMany(celebrities)
+.then(data => {
+  console.log(`Success! Added ${data.length} celebrities to the database`);
+  mongoose.connection.close();
+})
+.catch(err => {
+  console.log(err);
+})
